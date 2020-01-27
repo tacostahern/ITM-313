71b5decfe210fc4d00f0f8fe7c420f8811365706
tacostahern <35584612+tacostahern@users.noreply.github.com>
2020-01-26 18:46:16 -0600

    creation of hw file and folders

---
+++ Homework/1-30-2020/HW_source_code.py
@@ -0,0 +1,13 @@
+'''
+Homework due 1/30/2020
+This program will accept an input book code, the book's single copy cost,
+the current number of volumes on hand, the prospective class enrollment,
+and data that indicates whether the book is required or recommended. and
+has or has not been used in the past. Each book is identified by one-letter
+subject code followed by an integer book code. The output should show all
+input information together with the number of books that must be ordered
+(if any), the total cost of all books ordered, and the expected profit if
+the store pays 80% of the list price.
+Author: Tony Acosta Hernandez
+Course: ITM 313
+'''
