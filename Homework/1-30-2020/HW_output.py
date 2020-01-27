1686431f3af23534c8d446a3d70a5b07a61da707
tacostahern <35584612+tacostahern@users.noreply.github.com>
2020-01-26 19:07:55 -0600

    Create HW_output.py

---
+++ Homework/1-30-2020/HW_output.py
@@ -0,0 +1,42 @@
+Python 3.8.1 (tags/v3.8.1:1b293b6, Dec 18 2019, 23:11:46) [MSC v.1916 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+= RESTART: C:/Users/tacos/Documents/ITM-313/Homework/1-30-2020/HW_source_code.py
+Hello user!
+Please enter the book code: f5
+Please enter the book's single copy cost: 12
+Please enter the current number of volumes: 12
+Please enter the prospective class enrollment: 32
+Is this book required or recommended? required
+Has the book been used or unused? used
+So, from the information entered: 
+The book code is f5
+>>> 
+= RESTART: C:/Users/tacos/Documents/ITM-313/Homework/1-30-2020/HW_source_code.py
+Hello user!
+Please enter the book code: f3
+Please enter the book's single copy cost: 13
+Please enter the current number of volumes: 20
+Please enter the prospective class enrollment: 40
+Is this book required or recommended? required
+Has the book been used or unused? unused
+So, from the information entered: 
+The book code is f3
+Traceback (most recent call last):
+  File "C:/Users/tacos/Documents/ITM-313/Homework/1-30-2020/HW_source_code.py", line 24, in <module>
+    print("The cost of the book is $%" % (book_cost))
+ValueError: incomplete format
+>>> 
+= RESTART: C:/Users/tacos/Documents/ITM-313/Homework/1-30-2020/HW_source_code.py
+Hello user!
+Please enter the book code: f3
+Please enter the book's single copy cost: 12
+Please enter the current number of volumes: 12
+Please enter the prospective class enrollment: 40
+Is this book required or recommended? required
+Has the book been used or unused? unused
+So, from the information entered: 
+The book code is f3
+The cost of the book is $12.00
+The number of volumes is 12
+>>> 
