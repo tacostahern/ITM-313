a4cde0ca12fa33159628ca3ab069f7e07ce3dd46
tacostahern <35584612+tacostahern@users.noreply.github.com>
2020-01-28 23:22:20 -0600

    changed files

--- Homework/1-30-2020/HW_output.py
+++ Homework/1-30-2020/HW_output.py
@@ -1,42 +1,102 @@
 Python 3.8.1 (tags/v3.8.1:1b293b6, Dec 18 2019, 23:11:46) [MSC v.1916 64 bit (AMD64)] on win32
 Type "help", "copyright", "credits" or "license()" for more information.
 >>> 
-= RESTART: C:/Users/tacos/Documents/ITM-313/Homework/1-30-2020/HW_source_code.py
+= RESTART: C:\Users\tacos\Documents\ITM-313\Homework\1-30-2020\HW_source_code.py
 Hello user!
 Please enter the book code: f5
-Please enter the book's single copy cost: 12
-Please enter the current number of volumes: 12
-Please enter the prospective class enrollment: 32
-Is this book required or recommended? required
-Has the book been used or unused? used
-So, from the information entered: 
-The book code is f5
->>> 
-= RESTART: C:/Users/tacos/Documents/ITM-313/Homework/1-30-2020/HW_source_code.py
-Hello user!
-Please enter the book code: f3
-Please enter the book's single copy cost: 13
+Please enter the book's single copy cost: 1.21
 Please enter the current number of volumes: 20
 Please enter the prospective class enrollment: 40
-Is this book required or recommended? required
-Has the book been used or unused? unused
+Is this book required or recommended? Required
+Has the book been used or unused? Used
 So, from the information entered: 
-The book code is f3
-Traceback (most recent call last):
-  File "C:/Users/tacos/Documents/ITM-313/Homework/1-30-2020/HW_source_code.py", line 24, in <module>
-    print("The cost of the book is $%" % (book_cost))
-ValueError: incomplete format
+The book code is f5
+The cost of the book is $1.21
+The number of volumes is 20
+Class enrollment is 40 students
+The book is Required and it is Used
+The amount of books that must be order is 20
+The total cost of the books is 24.20
 >>> 
-= RESTART: C:/Users/tacos/Documents/ITM-313/Homework/1-30-2020/HW_source_code.py
+= RESTART: C:\Users\tacos\Documents\ITM-313\Homework\1-30-2020\HW_source_code.py
 Hello user!
-Please enter the book code: f3
-Please enter the book's single copy cost: 12
+Please enter the book code: d2
+Please enter the book's single copy cost: 32
 Please enter the current number of volumes: 12
-Please enter the prospective class enrollment: 40
+Please enter the prospective class enrollment: 20
+Is this book required or recommended? recommended
+Has the book been used or unused? unused
+So, from the information entered: 
+The book code is d2
+The cost of the book is $32.00
+The number of volumes is 12
+Class enrollment is 20 students
+The book is recommended and it is unused
+The amount of books that must be order is 8
+The total cost of the books is 256.00
+The expected profit is 51.20
+>>> 
+= RESTART: C:\Users\tacos\Documents\ITM-313\Homework\1-30-2020\HW_source_code.py
+Hello user!
+Please enter the book code: d3
+Please enter the book's single copy cost: 50
+Please enter the current number of volumes: 1
+Please enter the prospective class enrollment: 50
 Is this book required or recommended? required
 Has the book been used or unused? unused
 So, from the information entered: 
-The book code is f3
-The cost of the book is $12.00
-The number of volumes is 12
+The book code is d3
+The cost of the book is $50.00
+The number of volumes is 1
+Class enrollment is 50 students
+The book is required and it is unused
+The amount of books that must be ordered is 49
+The total cost of the books is $2450.00
+The expected profit is $490.00
+>>> 
+= RESTART: C:\Users\tacos\Documents\ITM-313\Homework\1-30-2020\HW_source_code.py
+Hello user!
+Please enter the book code: a1
+Please enter the book's single copy cost: a
+Traceback (most recent call last):
+  File "C:\Users\tacos\Documents\ITM-313\Homework\1-30-2020\HW_source_code.py", line 16, in <module>
+    book_cost = eval(input("Please enter the book's single copy cost: "))
+  File "<string>", line 1, in <module>
+NameError: name 'a' is not defined
+>>> 
+= RESTART: C:\Users\tacos\Documents\ITM-313\Homework\1-30-2020\HW_source_code.py
+Hello user!
+Please enter the book code: d2
+Please enter the book's single copy cost: 120
+Please enter the current number of volumes: 4605
+Please enter the prospective class enrollment: 10
+Is this book required or recommended? recommended
+Has the book been used or unused? unused
+So, from the information entered: 
+The book code is d2
+The cost of the book is $120.00
+The number of volumes is 4605
+Class enrollment is 10 students
+The book is recommended and it is unused
+The amount of books that must be ordered is 0
+The total cost of the books is $0.00
+The expected profit is $0.00
+>>> 
+= RESTART: C:\Users\tacos\Documents\ITM-313\Homework\1-30-2020\HW_source_code.py
+Hello user!
+Please enter the book code: h1
+Please enter the book's single copy cost: 23.34
+Please enter the current number of volumes: 20
+Please enter the prospective class enrollment: 21
+Is this book required or recommended? recommended
+Has the book been used or unused? used
+So, from the information entered: 
+The book code is h1
+The cost of the book is $23.34
+The number of volumes is 20
+Class enrollment is 21 students
+The book is recommended and it is used
+The amount of books that must be ordered is 1
+The total cost of the books is $23.34
+The expected profit is $4.67
 >>> 
