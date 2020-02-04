d8ae8219813cbc0e924c8a68d267fb07407c6601
tacostahern <35584612+tacostahern@users.noreply.github.com>
2020-02-03 21:38:42 -0600

    Create HW_source_code.py

---
+++ Homework/2-6-2020/HW_source_code.py
@@ -0,0 +1,8 @@
+'''
+This program will ask the user to select an option from a menu consisting
+of water, air, and steel. They will then be asked to enter the distance a
+sound wave will travel the selected medium, at which point the program will
+display the amount of time it will take.
+Author: Tony Acosta Hernandez
+Course: ITM 313
+'''
