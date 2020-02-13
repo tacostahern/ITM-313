c2936ac4c5bef9f65d49f7c7a898b89404a8830b
tacostahern <35584612+tacostahern@users.noreply.github.com>
2020-02-12 18:19:10 -0600

    files

---
+++ In class/2-6-2020Output.py
@@ -0,0 +1,43 @@
+Python 3.8.1 (tags/v3.8.1:1b293b6, Dec 18 2019, 23:11:46) [MSC v.1916 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+=== RESTART: C:/Users/tacos/Documents/ITM-313/In class/2-6-2020Source_code.py ==
+Please enter the weight of the package in pounds1
+The total shipping charge is $1.10
+>>> 
+=== RESTART: C:/Users/tacos/Documents/ITM-313/In class/2-6-2020Source_code.py ==
+Please enter the weight of the package in pounds: 1
+The total shipping charge is $1.10
+>>> 
+=== RESTART: C:/Users/tacos/Documents/ITM-313/In class/2-6-2020Source_code.py ==
+Please enter the weight of the package in pounds: 2
+The total shipping charge is $2.20
+>>> 
+=== RESTART: C:/Users/tacos/Documents/ITM-313/In class/2-6-2020Source_code.py ==
+Please enter the weight of the package in pounds: 3
+The total shipping charge is $6.60
+>>> 
+=== RESTART: C:/Users/tacos/Documents/ITM-313/In class/2-6-2020Source_code.py ==
+Please enter the weight of the package in pounds: 10
+The total shipping charge is $37.00
+>>> 
+=== RESTART: C:/Users/tacos/Documents/ITM-313/In class/2-6-2020Source_code.py ==
+Please enter the weight of the package in pounds: 20
+The total shipping charge is $76.00
+>>> 
+=== RESTART: C:/Users/tacos/Documents/ITM-313/In class/2-6-2020Source_code.py ==
+Please enter the weight of the package in pounds: 1.6648452
+The total shipping charge is $1.83
+>>> 
+=== RESTART: C:/Users/tacos/Documents/ITM-313/In class/2-6-2020Source_code.py ==
+Please enter the weight of the package in pounds: 0
+The total shipping charge is $0.00
+>>> 
+=== RESTART: C:/Users/tacos/Documents/ITM-313/In class/2-6-2020Source_code.py ==
+Please enter the weight of the package in pounds: 1000
+The total shipping charge is $3800.00
+>>> 
+=== RESTART: C:/Users/tacos/Documents/ITM-313/In class/2-6-2020Source_code.py ==
+Please enter the weight of the package in pounds: 1245.15652866
+The total shipping charge is $4731.59
+>>> 
---
+++ In class/2-6-2020Source_code.py
@@ -0,0 +1,23 @@
+'''
+This program will ask the user to enter the weight of a package then calculate
+and display the shipping charges based on the following criteria
+2 pounds or less : $1.10
+Over 2 pounds but no more than 6 pounds : $2.20
+Over 6 pounds but no more than 10 pounds : $3.70
+Over 10 pounds : $3.80
+Author: Tony Acosta Hernandez
+Course: ITM 313
+'''
+
+pounds = eval(input("Please enter the weight of the package in pounds: "))
+
+if (pounds <= 2):
+    charges = pounds * 1.10
+elif (pounds > 2) and (pounds <= 6):
+    charges = pounds * 2.20
+elif (pounds > 6) and (pounds <= 10):
+    charges = pounds * 3.7
+else:
+    charges = pounds * 3.8
+
+print("The total shipping charge is $%.2f" % (charges))
