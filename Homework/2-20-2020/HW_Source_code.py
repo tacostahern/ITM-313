6d3f5d6d267d34d41c494edf12f5c840961d0dc1
tacostahern <35584612+tacostahern@users.noreply.github.com>
2020-02-19 18:55:03 -0600

    Update HW_Source_code.py

--- Homework/2-20-2020/HW_Source_code.py
+++ Homework/2-20-2020/HW_Source_code.py
@@ -10,46 +10,48 @@ Course: ITM 313
 '''
 
 restart = 'y' #This will be our variable for restarting the program
-rmoi = 0
+rmoi = 0 #Creating rectangular moment of inertia variable
 
-while (restart.lower() == 'y'):
+while (restart.lower() == 'y'): #Main while loop using restart as its condition
     print("Thank you for choosing this program to help you with Rectangular Moment of Inertia!")
     print("Please choose from the following options:")
     print("Type 'I' for I-beam")
     print("Type 'R' for rectangular beam")
     print("Type 'C' for cylindrical beam")
-    beam = input("")
+    beam = input("") #This will display the menu and then allow the user to enter their option on the next line
 
-    if (beam.lower() == 'i' or beam.lower() == 'r'):
+    if (beam.lower() == 'i' or beam.lower() == 'r'): #Because i and r use 2 of the same variables, I have elected to go with this if statement to include both
         b = eval(input("Please enter the value for 'b' in inches: "))
-        while (b <= 0):
+        while (b <= 0): #Small while loop that will check for invalid input i.e. 0 or less
             b = eval(input("Invalid. Please enter the value for 'b' in inches: "))
             
         h = eval(input("Please enter the value for 'h' in inches: "))
-        while (h <= 0):
+        while (h <= 0): #Small while loop that will check for invalid input i.e. 0 or less
             h = eval(input("Invalid. Please enter the value for 'h' in inches: "))
 
-        if (beam.lower() == 'i'):
+        if (beam.lower() == 'i'): #No if they have chosen I-beams, they will be asked for B and H
             B = eval(input("Please enter the value for 'B' in inches: "))
-            while (B <= 0):
+            while (B <= 0): #Small while loop that will check for invalid input i.e. 0 or less
                 B = eval(input("Invalid. Please enter the value for 'B' in inches: "))
 
             H = eval(input("Please enter the value for 'H' in inches: "))
-            while (H <= 0):
+            while (H <= 0): #Small while loop that will check for invalid input i.e. 0 or less
                 H = eval(input("Invalid. Please enter the value for 'H' in inches: "))
-            rmoi = ((B * (H**3)) - (b * (h**3)))/12
+                
+            rmoi = ((B * (H**3)) - (b * (h**3)))/12 #Calculating Rectangular Moment of Inertia for I-beam
             print("Given b =", b, "inches, h =", h, "inches, B =", B, "inches, and H =", H, "inches,")
             print("The Rectangular Moment of Inertia for this I-beam is", rmoi,)
-        else:
-            rmoi = (b * (h**3))/12
+        else: #If they chose rectangular beams, we automatically go to calculate Rectangular Moment of Inertia
+            rmoi = (b * (h**3))/12 #Formula to get Rectangular moment of inertia for rectangular beams
             print("Given b =", b, "inches and h =", h, "inches,")
             print("The Rectangular Moment of Inertia for this rectangular beam is", rmoi,)
-    elif (beam.lower() == 'c'):
+    elif (beam.lower() == 'c'): #Otherwise if the user chooses a circular beam, we just ask for r
         r = eval(input("Please enter the value for r in inches: "))
-        while (r <= 0):
+        while (r <= 0):#Small while loop that will check for invalid input i.e. 0 or less
             r = eval(input("Invalid. Please enter the value for r in inches: "))
-        rmoi = (3.14 * (r**4))/4
+            
+        rmoi = (3.14 * (r**4))/4 #Formula to calculate Rectangular moment of inertia for circular beams
         print("Given r =", r, "inches,")
         print("The Rectangular Moment of Inertia of the circular beam is", rmoi)
     
-    restart = input("Would you like to try again? <y/n>? ")
+    restart = input("Would you like to try again? <y/n>? ") #We are asking the user if they want to restart
