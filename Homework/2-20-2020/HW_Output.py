09604ca380deaaf00372dbd9e098a77608920cea
tacostahern <35584612+tacostahern@users.noreply.github.com>
2020-02-20 20:32:28 -0600

    Create HW_Output.py

---
+++ Homework/2-20-2020/HW_Output.py
@@ -0,0 +1,78 @@
+Python 3.8.1 (tags/v3.8.1:1b293b6, Dec 18 2019, 23:11:46) [MSC v.1916 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+= RESTART: C:\Users\tacos\Documents\ITM-313\Homework\2-20-2020\HW_Source_code.py
+Thank you for choosing this program to help you with Rectangular Moment of Inertia!
+Please choose from the following options:
+Type 'I' for I-beam
+Type 'R' for rectangular beam
+Type 'C' for cylindrical beam
+i
+Please enter the value for 'b' in inches: 2
+Please enter the value for 'h' in inches: 0
+Invalid. Please enter the value for 'h' in inches: 0
+Invalid. Please enter the value for 'h' in inches: -2
+Invalid. Please enter the value for 'h' in inches: 2
+Please enter the value for 'B' in inches: 4
+Please enter the value for 'H' in inches: -4
+Invalid. Please enter the value for 'H' in inches: 0
+Invalid. Please enter the value for 'H' in inches: 4
+Given b = 2 inches, h = 2 inches, B = 4 inches, and H = 4 inches,
+The Rectangular Moment of Inertia for this I-beam is 20.0
+Would you like to try again? <y/n>? y
+Thank you for choosing this program to help you with Rectangular Moment of Inertia!
+Please choose from the following options:
+Type 'I' for I-beam
+Type 'R' for rectangular beam
+Type 'C' for cylindrical beam
+r
+Please enter the value for 'b' in inches: 0
+Invalid. Please enter the value for 'b' in inches: 0
+Invalid. Please enter the value for 'b' in inches: 2
+Please enter the value for 'h' in inches: 2
+Given b = 2 inches and h = 2 inches,
+The Rectangular Moment of Inertia for this rectangular beam is 1.3333333333333333
+Would you like to try again? <y/n>? y
+Thank you for choosing this program to help you with Rectangular Moment of Inertia!
+Please choose from the following options:
+Type 'I' for I-beam
+Type 'R' for rectangular beam
+Type 'C' for cylindrical beam
+c
+Please enter the value for r in inches: 0
+Invalid. Please enter the value for r in inches: 0
+Invalid. Please enter the value for r in inches: -2
+Invalid. Please enter the value for r in inches: -5555
+Invalid. Please enter the value for r in inches: 3
+Given r = 3 inches,
+The Rectangular Moment of Inertia of the circular beam is 63.585
+Would you like to try again? <y/n>? n
+>>> 
+= RESTART: C:\Users\tacos\Documents\ITM-313\Homework\2-20-2020\HW_Source_code.py
+Thank you for choosing this program to help you with Rectangular Moment of Inertia!
+Please choose from the following options:
+Type 'I' for I-beam
+Type 'R' for rectangular beam
+Type 'C' for cylindrical beam
+a
+Would you like to try again? <y/n>? a
+>>> 
+==== RESTART: C:\Users\tacos\Documents\ITM-313\Homework\2-20-2020\HW_Source_code.py ====
+Thank you for choosing this program to help you with Rectangular Moment of Inertia!
+Please choose from the following options:
+Type 'I' for I-beam
+Type 'R' for rectangular beam
+Type 'C' for cylindrical beam
+i
+Please enter the value for 'b' in inches: -1
+Invalid. Please enter the value for 'b' in inches: 1
+Please enter the value for 'h' in inches: -2
+Invalid. Please enter the value for 'h' in inches: 2
+Please enter the value for 'B' in inches: -3
+Invalid. Please enter the value for 'B' in inches: 3
+Please enter the value for 'H' in inches: 0
+Invalid. Please enter the value for 'H' in inches: 4
+Given b = 1 inches, h = 2 inches, B = 3 inches, and H = 4 inches,
+The Rectangular Moment of Inertia for this I-beam is 15.333333333333334
+Would you like to try again? <y/n>? N
+>>> 
