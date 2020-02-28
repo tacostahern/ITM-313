3002a027e8dcfa145a0d93beac4ad998ffc8689e
tacostahern <35584612+tacostahern@users.noreply.github.com>
2020-02-27 22:01:47 -0600

    added pdf

---
+++ Homework/2-27-2020/HW_Output.py
@@ -0,0 +1,55 @@
+Python 3.8.1 (tags/v3.8.1:1b293b6, Dec 18 2019, 23:11:46) [MSC v.1916 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+= RESTART: C:\Users\tacos\Documents\ITM-313\Homework\2-27-2020\HW_Source_code.py
+Please enter any integer number, or enter -99999 to stop: 1
+Please enter any integer number, or enter -99999 to stop: 2
+Please enter any integer number, or enter -99999 to stop: 3
+Please enter any integer number, or enter -99999 to stop: 4
+Please enter any integer number, or enter -99999 to stop: 5
+Please enter any integer number, or enter -99999 to stop: 6
+Please enter any integer number, or enter -99999 to stop: 7
+Please enter any integer number, or enter -99999 to stop: 8
+Please enter any integer number, or enter -99999 to stop: 9
+Please enter any integer number, or enter -99999 to stop: -99999
+The sum of all numbers entered is 45
+The average of all the numbers entered is 5.0
+The smallest number entered is 1
+The largest number entered is 9
+>>> 
+= RESTART: C:\Users\tacos\Documents\ITM-313\Homework\2-27-2020\HW_Source_code.py
+Please enter any integer number, or enter -99999 to stop: 12
+Please enter any integer number, or enter -99999 to stop: -12
+Please enter any integer number, or enter -99999 to stop: -99999
+The sum of all numbers entered is 0
+The average of all the numbers entered is 0.0
+The smallest number entered is -12
+The largest number entered is 12
+>>> 
+= RESTART: C:\Users\tacos\Documents\ITM-313\Homework\2-27-2020\HW_Source_code.py
+Please enter any integer number, or enter -99999 to stop: -1
+Please enter any integer number, or enter -99999 to stop: -2
+Please enter any integer number, or enter -99999 to stop: -3
+Please enter any integer number, or enter -99999 to stop: -4
+Please enter any integer number, or enter -99999 to stop: -5
+Please enter any integer number, or enter -99999 to stop: -6
+Please enter any integer number, or enter -99999 to stop: -7
+Please enter any integer number, or enter -99999 to stop: -8
+Please enter any integer number, or enter -99999 to stop: -9
+Please enter any integer number, or enter -99999 to stop: -99999
+The sum of all numbers entered is -45
+The average of all the numbers entered is -5.0
+The smallest number entered is -9
+The largest number entered is -1
+>>> 
+= RESTART: C:\Users\tacos\Documents\ITM-313\Homework\2-27-2020\HW_Source_code.py
+Please enter any integer number, or enter -99999 to stop: 123
+Please enter any integer number, or enter -99999 to stop: 456
+Please enter any integer number, or enter -99999 to stop: 789
+Please enter any integer number, or enter -99999 to stop: -123
+Please enter any integer number, or enter -99999 to stop: -99999
+The sum of all numbers entered is 1245
+The average of all the numbers entered is 311.25
+The smallest number entered is -123
+The largest number entered is 789
+>>> 
