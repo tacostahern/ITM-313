7621ec70c2344441d102f541dae1bd5d3ff398fa
tacostahern <35584612+tacostahern@users.noreply.github.com>
2020-03-19 21:38:57 -0500

    Create 3-19-2020Outout.py

---
+++ In class/3-19-2020Outout.py
@@ -0,0 +1,105 @@
+Python 3.8.1 (tags/v3.8.1:1b293b6, Dec 18 2019, 23:11:46) [MSC v.1916 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+== RESTART: C:\Users\tacos\Documents\ITM-313\In class\3-19-2020Source_code.py ==
+Please enter any number: 1
+Please enter any number: 2
+Please enter any number: 3
+Please enter any number: 4
+Please enter any number: 5
+Please enter any number: 6
+Please enter any number: 7
+Please enter any number: 
+Traceback (most recent call last):
+  File "C:\Users\tacos\Documents\ITM-313\In class\3-19-2020Source_code.py", line 17, in <module>
+    num = eval(input("Please enter any number: "))
+  File "<string>", line 0
+    
+    ^
+SyntaxError: unexpected EOF while parsing
+>>> 
+== RESTART: C:\Users\tacos\Documents\ITM-313\In class\3-19-2020Source_code.py ==
+Please enter any number: 1
+Please enter any number: 2
+Please enter any number: 3
+Please enter any number: 4
+Please enter any number: 5
+Please enter any number: 6
+Please enter any number: 7
+Please enter any number: 8
+Please enter any number: 9
+Please enter any number: 10
+Entered: 1 Distance from average: -4.5
+Entered: 2 Distance from average: -3.5
+Entered: 3 Distance from average: -2.5
+Entered: 4 Distance from average: -1.5
+Entered: 5 Distance from average: -0.5
+Entered: 6 Distance from average: 0.5
+Entered: 7 Distance from average: 1.5
+Entered: 8 Distance from average: 2.5
+Entered: 9 Distance from average: 3.5
+Entered: 10 Distance from average: 4.5
+>>> 
+== RESTART: C:\Users\tacos\Documents\ITM-313\In class\3-19-2020Source_code.py ==
+Please enter any number: 1
+Please enter any number: 1
+Please enter any number: 1
+Please enter any number: 1
+Please enter any number: 1
+Please enter any number: 1
+Please enter any number: 1
+Please enter any number: 1
+Please enter any number: 1
+Please enter any number: 1
+Entered: 1 Distance from average: 0.0
+Entered: 1 Distance from average: 0.0
+Entered: 1 Distance from average: 0.0
+Entered: 1 Distance from average: 0.0
+Entered: 1 Distance from average: 0.0
+Entered: 1 Distance from average: 0.0
+Entered: 1 Distance from average: 0.0
+Entered: 1 Distance from average: 0.0
+Entered: 1 Distance from average: 0.0
+Entered: 1 Distance from average: 0.0
+>>> 
+== RESTART: C:\Users\tacos\Documents\ITM-313\In class\3-19-2020Source_code.py ==
+Please enter any number: -99999
+Error
+>>> 
+== RESTART: C:\Users\tacos\Documents\ITM-313\In class\3-19-2020Source_code.py ==
+Please enter any number: 1
+Please enter any number: 2
+Please enter any number: 3
+Please enter any number: -99999
+Entered: 1 Distance from average: -1.0
+Entered: 2 Distance from average: 0.0
+Entered: 3 Distance from average: 1.0
+>>> 
+== RESTART: C:\Users\tacos\Documents\ITM-313\In class\3-19-2020Source_code.py ==
+Please enter any number: -1
+Please enter any number: -2
+Please enter any number: -3
+Please enter any number: -99999
+Entered: -1 Distance from average: 1.0
+Entered: -2 Distance from average: 0.0
+Entered: -3 Distance from average: -1.0
+>>> 
+== RESTART: C:\Users\tacos\Documents\ITM-313\In class\3-19-2020Source_code.py ==
+Please enter any number: 2
+Please enter any number: 2
+Please enter any number: 2
+Please enter any number: 2
+Please enter any number: 2
+Please enter any number: 2
+Please enter any number: 2
+Please enter any number: 4
+Please enter any number: -99999
+Entered: 2 Distance from average: -0.25
+Entered: 2 Distance from average: -0.25
+Entered: 2 Distance from average: -0.25
+Entered: 2 Distance from average: -0.25
+Entered: 2 Distance from average: -0.25
+Entered: 2 Distance from average: -0.25
+Entered: 2 Distance from average: -0.25
+Entered: 4 Distance from average: 1.75
+>>> 
