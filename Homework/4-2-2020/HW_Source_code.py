96913313e63fbcc32311fb7716c3b0c1b9f98728
tacostahern <35584612+tacostahern@users.noreply.github.com>
2020-03-30 13:55:10 -0500

    Update HW_Source_code.py

--- Homework/4-2-2020/HW_Source_code.py
+++ Homework/4-2-2020/HW_Source_code.py
@@ -5,13 +5,36 @@ print them out
 Author: Tony Acosta Hernandez
 Course: ITM 313
 '''
-import os
+import os, statistics
+
+sum = 0
+mean = 0
+median = 0
 
 f1 = "data1.txt"
 
 if os.path.isfile(f1):
     infile = open(f1, "r")
 
-    s = infile.read()
+    s1 = infile.read()
+
+    num1 = [ eval(x) for x in s1.split() ]
+    for x in num1:
+        sum += x
+    mean = sum/len(num1)
+    print("The sum for data1.txt is", sum)
+    print("The mean is", mean)
+    
 else:
-    print("No such file exists")
+    print("data1.txt does not exists")
+
+f2 = "data2.txt"
+
+if os.path.isfile(f2):
+    infile = open(f2, "r")
+
+    s2 = infile.read()
+
+    
+else:
+    print("data2.txt does not exists")
