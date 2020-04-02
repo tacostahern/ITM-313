3766f793c08c378bb07bc6515980ccd982f00644
tacostahern <35584612+tacostahern@users.noreply.github.com>
2020-04-02 18:33:40 -0500

    Create HW_Output.py

---
+++ Homework/4-2-2020/HW_Output.py
@@ -0,0 +1,17 @@
+Python 3.8.1 (tags/v3.8.1:1b293b6, Dec 18 2019, 23:11:46) [MSC v.1916 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+= RESTART: C:\Users\tacos\Documents\ITM-313\Homework\4-2-2020\HW_Source_code.py
+The sum for data1.txt is 1976.1
+The mean is 197.60999999999999
+The median is 16.0
+The smallest value is -10
+The largest value is 999
+Total numbers read is 10
+The sum for data2.txt is 374434
+The mean is 499.24533333333335
+The median is 487.5
+The smallest value is 6
+The largest value is 1000
+Total numbers read is 750
+>>> 
