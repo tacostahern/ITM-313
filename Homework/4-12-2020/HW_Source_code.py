9a70e9b0f5e0adbcddc327db7ed2e30a504a5b64
tacostahern <35584612+tacostahern@users.noreply.github.com>
2020-04-12 16:57:56 -0500

    Update HW_Source_code.py

--- Homework/4-12-2020/HW_Source_code.py
+++ Homework/4-12-2020/HW_Source_code.py
@@ -14,7 +14,7 @@ class Pet:
     def set_name(self, name):
         self.__name = name
 
-    def set_animaml_type(self, animal_type):
+    def set_animal_type(self, animal_type):
         self.__animal_type = animal_type
 
     def set_age(self, age):
