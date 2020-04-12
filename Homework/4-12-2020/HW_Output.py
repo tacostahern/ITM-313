7db13d150e0eb290b85b85eda95c8a2a892b0417
tacostahern <35584612+tacostahern@users.noreply.github.com>
2020-04-12 18:01:36 -0500

    a

---
+++ Homework/4-12-2020/HW_Output.py
@@ -0,0 +1,19 @@
+Python 3.8.1 (tags/v3.8.1:1b293b6, Dec 18 2019, 23:11:46) [MSC v.1916 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+= RESTART: C:\Users\tacos\Documents\ITM-313\Homework\4-12-2020\HW_Source_code.py
+Please enter the name of your pet: as
+Please enter the type of animal your pet is: .asd
+Please enter the age of your pet: 3
+Your pet's name is as
+Your pet is a .asd
+Your pet is 3 year old
+>>> 
+= RESTART: C:\Users\tacos\Documents\ITM-313\Homework\4-12-2020\HW_Source_code.py
+Please enter the name of your pet: Azul
+Please enter the type of animal your pet is: Dog
+Please enter the age of your pet: 1
+Your pet's name is Azul
+Your pet is a Dog
+Your pet is 1 year old
+>>> 
