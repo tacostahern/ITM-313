197725d3b928eede57c55d78641867a7d376b33e
tacostahern <35584612+tacostahern@users.noreply.github.com>
2020-04-25 16:20:10 -0500

    ssad

---
+++ Homework/4-26-2020/HW_Source_code.py
@@ -0,0 +1,42 @@
+'''
+The following prograg will be for GUI practice. It will ask the user to enter
+their full name, field major, and number of semesters in school. They will also
+be asked to enter two numbers. The sum and product of the two numbers will be
+calculated. Finally, all input will be printed out into one window box
+Author: Tony Acosta Hernandez
+Course ITM 313
+'''
+
+import tkinter as tk
+from tkinter import messagebox
+from tkinter import simpledialog
+
+root = tk.Tk()
+root.withdraw()
+
+def main():
+    name = simpledialog.askstring("Full Name", "Enter your name")
+    result = "Your name is: " + name
+
+    fm = simpledialog.askstring("Field Major", "Enter your field major")
+    result = result + "\nYour field major is: " + fm
+
+    numSemesters = simpledialog.askstring("Number of Semesters", "Enter your number of semesters")
+    result = result + "\nThe number of semesters you have had in school is: " + numSemesters
+    
+    num1 = simpledialog.askstring("First Number", "Enter a number")
+    result = result + "\nThe first number you entered is: " + num1
+
+    num2 = simpledialog.askstring("Second Number", "Enter another number")
+    result = result + "\nThe second number you entered is: " + num2
+
+    summ = int(num1) + int(num2)
+    product = int(num1) * int(num2)
+
+    result = result + "\nThe sum of the two numbers is: " + str(summ)
+    result = result + "\nThe product of the two numbers is: " + str(product)
+    
+    messagebox.showinfo("Display Results", result)
+
+
+main()
---
+++ In class/guiPractice.py
@@ -0,0 +1,15 @@
+import tkinter as tk
+from tkinter import messagebox
+from tkinter import simpledialog
+root = tk.Tk()
+root.withdraw()
+answer = True
+while (answer):
+    name = simpledialog.askstring("Type your full name", "Enter your name")
+    result = "Your name is: " + name
+    idNumber = simpledialog.askinteger("Input four digit ID number", "Enter ID#")
+    result = result + "\n" + "Your ID# is: " + str(idNumber) + "\n"
+    cost = simpledialog.askfloat("Input cost per credit hour", "Enter cost per credit")
+    result = result + "Cost per credit hour is: " + str(cost) + "\n"
+    messagebox.showinfo("Display Results", result)
+    answer = messagebox.askyesno("Ask Again", "Do you want to re-type the input")
