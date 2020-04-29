57d5d3c4d25c4a92dd77eea91e3c8952a9005983
tacostahern <35584612+tacostahern@users.noreply.github.com>
2020-04-29 15:58:43 -0500

    added all code for classes

--- Final Project/Source_code.py
+++ Final Project/Source_code.py
@@ -20,19 +20,55 @@ class Circle:
         return math.pi * (self.__radius ** 2)
 
     def display(self):
-        return ("Circle has a radius of", self.find_area())
+        return "Circle has an area of %.2f" % (self.find_area())
     
 
 class Square:
     def __init__(self, side = 2.5):
         self.__side = side
 
+    def get_side(self):
+        return self.__side
+
+    def set_side(self, side):
+        self.__side = side
+
+    def find_area(self):
+        return self.__side ** 2
+
+    def display(self):
+        return "Square has an area of %.2f" % (self.find_area())
+
 class Rectangle:
     def __init__(self, length = 2.5, width = 3):
         self.__length, self.__width = length, width
 
+    def get_length(self):
+        return self.__length
+
+    def get_width(self):
+        return self.__width
+
+    def set_length(self, length):
+        self.__length = length
+
+    def set_width(self, width):
+        self.__width = width
+
+    def find_area(self):
+        return self.__length * self.__width
+
+    def display(self):
+        return "Rectangle has an area of %.2f" % (self.find_area())
+
 def main():
-    Circle circle = new Circle()
+    circle = Circle()
     print(circle.display())
+
+    square = Square()
+    print(square.display())
+
+    rectangle = Rectangle()
+    print(rectangle.display())
 
 main()
