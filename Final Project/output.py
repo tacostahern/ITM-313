b025bbde878840d9a12abae84c2587892384ecc3
tacostahern <35584612+tacostahern@users.noreply.github.com>
2020-04-30 15:53:44 -0500

    a

---
+++ Final Project/output.py
@@ -0,0 +1,126 @@
+Python 3.8.1 (tags/v3.8.1:1b293b6, Dec 18 2019, 23:11:46) [MSC v.1916 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+==== RESTART: C:\Users\tacos\Documents\ITM-313\Final Project\Source_code.py ====
+Rectangle has an area of 7.50
+Rectangle has an area of 7.50
+Rectangle has an area of 7.50
+Square has an area of 6.25
+Square has an area of 6.25
+Rectangle has an area of 7.50
+Circle has an area of 12.57
+Square has an area of 6.25
+Circle has an area of 12.57
+Rectangle has an area of 7.50
+>>> 
+==== RESTART: C:\Users\tacos\Documents\ITM-313\Final Project\Source_code.py ====
+Rectangle has an area of 7.50
+Rectangle has an area of 7.50
+Rectangle has an area of 7.50
+Square has an area of 6.25
+Square has an area of 6.25
+Circle has an area of 12.57
+Circle has an area of 12.57
+Square has an area of 6.25
+Circle has an area of 12.57
+Rectangle has an area of 7.50
+>>> 
+==== RESTART: C:\Users\tacos\Documents\ITM-313\Final Project\Source_code.py ====
+Circle has an area of 12.57
+Square has an area of 6.25
+Square has an area of 6.25
+Circle has an area of 12.57
+Circle has an area of 12.57
+Rectangle has an area of 7.50
+Square has an area of 6.25
+Circle has an area of 12.57
+Square has an area of 6.25
+Rectangle has an area of 7.50
+>>> 
+==== RESTART: C:\Users\tacos\Documents\ITM-313\Final Project\Source_code.py ====
+Rectangle has an area of 7.50
+Circle has an area of 12.57
+Circle has an area of 12.57
+Circle has an area of 12.57
+Rectangle has an area of 7.50
+Rectangle has an area of 7.50
+Square has an area of 6.25
+Rectangle has an area of 7.50
+Circle has an area of 12.57
+Square has an area of 6.25
+>>> 
+==== RESTART: C:\Users\tacos\Documents\ITM-313\Final Project\Source_code.py ====
+Rectangle has an area of 7.50
+Square has an area of 6.25
+Circle has an area of 12.57
+Square has an area of 6.25
+Rectangle has an area of 7.50
+Rectangle has an area of 7.50
+Rectangle has an area of 7.50
+Rectangle has an area of 7.50
+Circle has an area of 12.57
+Rectangle has an area of 7.50
+>>> 
+Circle has an area of 12.57==== RESTART: C:\Users\tacos\Documents\ITM-313\Final Project\Source_code.py ====
+Circle has an area of 12.57
+Square has an area of 6.25
+Rectangle has an area of 7.50
+Circle has an area of 12.57
+Circle has an area of 12.57
+Circle has an area of 12.57
+Square has an area of 6.25
+Square has an area of 6.25
+Circle has an area of 12.57
+Circle has an area of 12.57
+>>> 
+>>> 
+
+==== RESTART: C:\Users\tacos\Documents\ITM-313\Final Project\Source_code.py ====
+>>> 
+Rectangle has an area of 7.50
+Square has an area of 6.25
+Rectangle has an area of 7.50
+Circle has an area of 12.57
+Circle has an area of 12.57
+Rectangle has an area of 7.50
+Circle has an area of 12.57
+Circle has an area of 12.57
+Circle has an area of 12.57
+Circle has an area of 12.57
+>>> 
+==== RESTART: C:\Users\tacos\Documents\ITM-313\Final Project\Source_code.py ====
+Rectangle has an area of 7.50
+Rectangle has an area of 7.50
+Rectangle has an area of 7.50
+Square has an area of 6.25
+Rectangle has an area of 7.50
+Circle has an area of 12.57
+Circle has an area of 12.57
+Rectangle has an area of 7.50
+Circle has an area of 12.57
+Circle has an area of 12.57
+>>> 
+==== RESTART: C:\Users\tacos\Documents\ITM-313\Final Project\Source_code.py ====
+Circle has an area of 12.57
+Square has an area of 6.25
+Rectangle has an area of 7.50
+Rectangle has an area of 7.50
+Circle has an area of 12.57
+Circle has an area of 12.57
+Square has an area of 6.25
+Circle has an area of 12.57
+Circle has an area of 12.57
+Rectangle has an area of 7.50
+>>> 
+==== RESTART: C:\Users\tacos\Documents\ITM-313\Final Project\Source_code.py ====
+Square has an area of 6.25
+Square has an area of 6.25
+Circle has an area of 12.57
+Circle has an area of 12.57
+Rectangle has an area of 7.50
+Square has an area of 6.25
+Rectangle has an area of 7.50
+Rectangle has an area of 7.50
+Square has an area of 6.25
+Circle has an area of 12.57
+>>> 
